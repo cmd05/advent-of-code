131850f59dfd860d3f1da57b171b820c1aadc0f8
cmd05 <splendid.snippet670@slmail.me>
2024-12-07 15:09:05 +0530

    cleanup some code

--- aoc-2024/day07/puzzle_2.cpp
+++ aoc-2024/day07/puzzle_2.cpp
@@ -32,7 +32,6 @@ struct Line {
 
 int main() {
     std::ifstream ifs {"input.txt"};
-
     std::vector<Line> lines;
 
     for(std::string line; std::getline(ifs, line);) {
@@ -61,44 +60,28 @@ int main() {
         ss_res >> line_data.result;
 
         // load nums
-        for(uint64_t x; ss >> x;) {
-            // std::cout << x << " ";
+        for(uint64_t x; ss >> x;)
             line_data.nums.push_back(x);
-        }
 
         lines.push_back(line_data);
-        // std::cout << '\n';
     }
 
     uint64_t final_sum = 0;
 
-    // take care of `int` overflow
-
     // check lines
     for(unsigned int i = 0; i < lines.size(); i++) {
         Line& line = lines[i];
-
-        // check how many operators needed
-        unsigned int num_ops = line.nums.size() - 1;
-
+        
         // "base-3 counting pattern"
         // + -> 0
         // * -> 1
         // || -> 2
+        unsigned int num_ops = line.nums.size() - 1;
         unsigned int num_seqs = pow(3, num_ops); // upper bound not included
-
-        // // rightmost bit => leftmost operation
-
-        // for(unsigned int op_seq = 0; op_seq < max_bin; op_seq++) {
-        //     for(int j = 0; j < num_ops; j++) {
-        //         if((op_seq >> j) & 1)
-        //     }
-        // }
         std::vector<std::string> operations;
 
-        for(unsigned int j = 0; j < num_seqs; j++) {
+        for(unsigned int j = 0; j < num_seqs; j++)
             operations.push_back(dec_to_base3(j, num_ops));
-        }
 
         bool res_equal = false;
         unsigned int seq_index_eq = 0;
@@ -109,43 +92,27 @@ int main() {
 
             // rightmost bit => leftmost operation and so on
             for(unsigned int j = 1; j < line.nums.size(); j++) {
-                uint64_t calc_tmp = calc_res;
-
                 if(sequence[j-1] == '0')
                     calc_res += line.nums[j];
                 else if(sequence[j-1] == '1')
                     calc_res *= line.nums[j];
-                else if(sequence[j-1] == '2') {
+                else if(sequence[j-1] == '2')
                     calc_res = std::stoull(std::to_string(calc_res) + std::to_string(line.nums[j]));
-                }
-                
-                if(calc_res < calc_tmp) {
-                    std::cout << ("OVERFLOW");
-                    exit(0);
-                }
 
                 // only break if last num i.e checked all numbers
                 if(calc_res == line.result && j == (line.nums.size() - 1)) {
                     res_equal = true;
                     seq_index_eq = seq_index;
                     goto loop_exit;
-                } else if(calc_res > line.result) {
+                } else if(calc_res > line.result)
                     break;
-                }
             }
         }
 
         loop_exit:
         if(res_equal) {
-            uint64_t tmp_final_sum = final_sum;
             final_sum += line.result;
-
-            if(final_sum <= tmp_final_sum) {
-                std::cout << ("OVERFLOW");
-                exit(0);
-            }
-
-            std::cout << "operation " << operations[seq_index_eq] << ". on line " << i << '\n';
+            // std::cout << "operation " << operations[seq_index_eq] << ". on line " << i << '\n';
         }
     }
 
