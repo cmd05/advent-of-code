e57c483805c69fefee67e7cc3b8922c1ee2736d9
cmd05 <splendid.snippet670@slmail.me>
2024-12-09 16:11:30 +0530

    add day09

--- day09/puzzle_2.py
+++ day09/puzzle_2.py
@@ -9,66 +9,45 @@ for i in range(len(disk_map)):
     if i % 2 == 0:
         id_map.append((id, int(disk_map[i])))
         id += 1
-    # elif int(disk_map[i]) != 0: # assuming free spaces only occur at odd positions
-    else:
+    elif int(disk_map[i]) != 0: # ignore zero free spaces
+    # else:
         id_map.append((free_space_indicator, int(disk_map[i])))
 
-# print(id_map)
-# print(len(id_map))
-# exit()
-
-# print(id_map)
-# unswapped_id_map = id_map
-
-# no need of recursively checking from last id each time according to example
-
 # swap free spaces
-i = len(id_map) - 1
-for i in range(len(id_map) - 1, -1, -1):
-    if id_map[i][0] == free_space_indicator:
+id_index = len(id_map) - 1
+
+while id_index >= 0:
+    if id_map[id_index][0] == free_space_indicator:
+        id_index -= 1
         continue
 
-    # print(id_map[i])
-
-    for j in range(i):
-        if id_map[j][0] == free_space_indicator and id_map[j][1] >= id_map[i][1]:
-            tmp_file = id_map[i]
+    for j in range(id_index):
+        if id_map[j][0] == free_space_indicator and id_map[j][1] >= id_map[id_index][1]:
+            tmp_file = id_map[id_index]
             tmp_free = id_map[j]
             
-            # print("move", id_map[i], "to", id_map[j])
-
-            # free space now at old file pos (step not required tho)
-            # we can merge that free space too (not reqd)
-            id_map[i] = (free_space_indicator, tmp_file[1])
-
             # move file
             id_map[j] = tmp_file
+
+            # free space now at old file pos
+            id_map[id_index] = (free_space_indicator, tmp_file[1])
 
             # if remaining space
             remaining = tmp_free[1] - tmp_file[1]
             if remaining != 0:
                 id_map.insert(j+1, (free_space_indicator, remaining))
-                # fix index if insertion happens
-                i += 1
+                id_index += 1 # fix index if insertion happens
 
-            # print("after move", id_map)
-            # if(tmp_file[0] == 4): 
-            #     print("exit")
-            #     exit()
             break
-
-# print(id_map)
+    
+    id_index -= 1
 
 pos = 0
-cum = 0
+ans = 0
 for i in range(len(id_map)):
-    if id_map[i][1] == 0:
-        print("zero")
     for j in range(id_map[i][1]):
         if id_map[i][0] != free_space_indicator:
-            cum += pos * id_map[i][0]
+            ans += pos * id_map[i][0]
         pos += 1
 
-print(cum)
-# 6402760149876 too high
-# 6363913128533 corr
+print(ans)
